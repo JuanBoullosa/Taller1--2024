98e7a140b058ce2c84d7d15e127f29c193f5d51a
JuanBoullosa <juanibou@gmail.com>
2024-02-23 12:29:52 -0300

    CargarExpresion

--- Proyecto Taller1/Expresiones.cpp
+++ Proyecto Taller1/Expresiones.cpp
@@ -1,6 +1,13 @@
 #include "Expresiones.h"
 
 
-CrearExpresion(ArbolExpresiones Aexp, Expresion &expre);
+CrearExpresion(ArbolExpresiones Aexp, Expresion &expre)
+
+{
+    int id=0;
+    expre.numero=id+1;
+    expre.arbol=Aexp;
+    id++;
+}
 
 
