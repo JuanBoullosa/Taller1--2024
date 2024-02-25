6f04df59fb87597da2a5709167e7023257a8046b
PabloSaravia7 <jp.saraviadeleon@gmail.com>
2024-02-25 18:55:47 -0300

    otro

--- Proyecto Taller1/main.cpp
+++ Proyecto Taller1/main.cpp
@@ -22,42 +22,47 @@ str expAnd;
 str expOr;
 CargarComandos(atomic,compound,show,evaluate,save,load,exit,expTrue,expFalse,expNot,expAnd,expOr);// Esto se puede poner en una lista de momento lo deje asi para achicar un poco
 
-//                     Esta es la parte principal del codigo
-str comando;                             // Declaramos string donde va a ingresar el usuario comando
-strcrear(comando);                      //Creamos el string comando
-ListaString Lista1;                     //Declaramos la lista que usaremos para evaluar los string
+//                                  Esta es la parte principal del codigo
+
+str comando;                                            // Declaramos string donde va a ingresar el usuario comando
+strcrear(comando);                                      //Creamos el string comando
+ListaString Lista1;                                     //Declaramos la lista que usaremos para evaluar los string
 CrearLista(Lista1);
 int contadorexp1=1;
 ListaExpresiones ListExpPrincipal;
 CrearListaExpresiones(ListExpPrincipal);
-                        // Ingreso de comando y division de palabras
-printf("Ingrese comando: ");            //Solicitamos al usuario que ingrese un comando
-scan(comando);                          // Scan dinamico para el ingreso de el string comando
-printf("\n");                          // Salto de linea
-partirString(comando, Lista1);          // Procedimiento para partir el string
+                                // Ingreso de comando y division de palabras
+
+printf("Ingrese comando: ");                            //Solicitamos al usuario que ingrese un comando
+scan(comando);                                          // Scan dinamico para el ingreso de el string comando
+printf("\n");                                           // Salto de linea
+partirString(comando, Lista1);                          // Procedimiento para partir el string
 printf("Mostrar Lista de string: \n");
-MostrarLista(Lista1);                   // Mostramos el string partido
+MostrarLista(Lista1);                                   // Mostramos el string partido
 printf("\n");
 
+                                    //Comando atomic
 
-                            //Comando atomic
-Atomic(Lista1, contadorexp1, ListExpPrincipal);  // Ejecutamos el comando atomic
-printf("\n");
-delete[] Lista1;
-CrearLista(Lista1);
+Atomic(Lista1, contadorexp1, ListExpPrincipal);         // Ejecutamos el comando atomic
 
-printf("Ingrese comando: ");            //Solicitamos al usuario que ingrese un comando
-scan(comando);                          // Scan dinamico para el ingreso de el string comando
-printf("\n");                          // Salto de linea
-partirString(comando, Lista1);          // Procedimiento para partir el string
+printf("\n");                                           //Salto de linea
+delete[] Lista1;                                        //Liberamos memoria de lista para ingresar nuevo comando
+CrearLista(Lista1);                                     //Asignamos memoria a lista
+printf("Ingrese comando: ");                            //Solicitamos al usuario que ingrese un comando
+scan(comando);                                          // Scan dinamico para el ingreso de el string comando
+printf("\n");                                           // Salto de linea
+partirString(comando, Lista1);                          // Procedimiento para partir el string
 printf("El valor de la expresion ingresa es: \n");
-Show(Lista1, ListExpPrincipal,show,contadorexp1);
+
+                                //Prueba del comand Show
+
+Show(Lista1, ListExpPrincipal,show,contadorexp1);       //Ejecutamos show en ejecucion
+
+
 
 
 
 //Estas variables deberian ir en el comando compound
-
-
                          //Creamos la lista
 Expresion expre;                    //Creamos  expresion
 ArbolExpresiones arbol;
